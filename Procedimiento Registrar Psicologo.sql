581f4cea7d5136a1b6bb2cb09a6f49d17821e751
Joje51 <jorge.li@utp.ac.pa>
2026-07-07 20:33:25 -0500

    Correciones pequeñas

---
+++ Procedimiento Registrar Psicologo.sql
@@ -0,0 +1,57 @@
+CREATE OR REPLACE PROCEDURE SP_REGISTRAR_PSICOLOGO(
+    P_ID_PSICOLOGO              OUT PSICOLOGO.ID_PSICOLOGO%TYPE,
+    P_PRIMER_NOMBRE             IN PSICOLOGO.PRIMER_NOMBRE_PSICOLOGO%TYPE,
+    P_PRIMER_APELLIDO           IN PSICOLOGO.PRIMER_APELLIDO_PSICOLOGO%TYPE,
+    P_CEDULA                    IN PSICOLOGO.CEDULA_PSICOLOGO%TYPE,
+    P_CORREO                    IN PSICOLOGO.CORREO_UTP%TYPE,
+    P_RESULTADO                 OUT VARCHAR2
+)
+IS
+    V_EXISTE NUMBER;
+BEGIN
+
+    -- Verificar si ya existe la cédula
+    SELECT COUNT(*)
+    INTO V_EXISTE
+    FROM PSICOLOGO
+    WHERE CEDULA_PSICOLOGO = P_CEDULA;
+
+    IF V_EXISTE > 0 THEN
+        P_RESULTADO := 'Error: Ya existe un psicólogo con esa cédula.';
+    ELSE
+        P_ID_PSICOLOGO := SEQ_PSICOLOGO.NEXTVAL;
+        INSERT INTO PSICOLOGO(
+            ID_PSICOLOGO,
+            PRIMER_NOMBRE_PSICOLOGO,
+            PRIMER_APELLIDO_PSICOLOGO,
+            CEDULA_PSICOLOGO,
+            CORREO_UTP
+        )
+        VALUES(
+            P_ID_PSICOLOGO,
+            P_PRIMER_NOMBRE,
+            P_PRIMER_APELLIDO,
+            P_CEDULA,
+            P_CORREO
+        );
+
+        COMMIT;
+
+        P_RESULTADO := 'Psicólogo registrado correctamente.';
+
+    END IF;
+
+EXCEPTION
+
+    WHEN DUP_VAL_ON_INDEX THEN
+        ROLLBACK;
+        P_ID_PSICOLOGO := NULL;
+        P_RESULTADO := 'Error: Registro duplicado.';
+
+    WHEN OTHERS THEN
+        ROLLBACK;
+        P_ID_PSICOLOGO := NULL;
+        P_RESULTADO := 'Error: ' || SQLERRM;
+
+END;
+/
